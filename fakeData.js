c2b204b76c3bb00518503ce50be26f0ca376da16
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-08-13 20:45:22 +0100

    initial commit

---
+++ fakeData.js
@@ -0,0 +1,27 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "Canada",
+  },
+  {
+    id: 2,
+    name: "James",
+    username: "james",
+    age: 23,
+    nationality: "Brazil",
+  },
+  {
+    id: 1,
+    name: "Jimmy",
+    username: "jimmy",
+    age: 21,
+    nationality: "Mexico",
+  },
+];
+
+module.exports = {
+    UserList
+}
