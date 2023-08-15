1423b0bc372fba5c417e0b295802f10ff3a1fca9
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-08-15 15:08:08 +0100

    worked on the delete mutation

--- fakeData.js
+++ fakeData.js
@@ -4,24 +4,81 @@ const UserList = [
     name: "John",
     username: "john",
     age: 20,
-    nationality: "Canada",
+    nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "James",
+        username: "james",
+        age: 23,
+        nationality: "BRAZIL",
+      },
+      {
+        id: 3,
+        name: "Jimmy",
+        username: "jimmy",
+        age: 21,
+        nationality: "MEXICO",
+      },
+    ],
+    
   },
   {
     id: 2,
     name: "James",
     username: "james",
     age: 23,
-    nationality: "Brazil",
+    nationality: "BRAZIL",
+    friends: [
+      {
+        id: 1,
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "CANADA",
+      },
+    ],
   },
   {
-    id: 1,
+    id: 3,
     name: "Jimmy",
     username: "jimmy",
     age: 21,
-    nationality: "Mexico",
+    nationality: "MEXICO",
+    friends: [
+      {
+        id: 2,
+        name: "James",
+        username: "james",
+        age: 23,
+        nationality: "BRAZIL",
+      },
+    ],
   },
 ];
 
+const MovieList = [
+  {
+    id: 1,
+    name: "avengers",
+    yearOfPublication: 2022,
+    isInTheatres: true
+  },
+  {
+    id: 2,
+    name: "justice league",
+    yearOfPublication: 2025,
+    isInTheatres: false
+  },
+  {
+    id: 3,
+    name: "star wars",
+    yearOfPublication: 2020,
+    isInTheatres: true
+  },
+]
+
 module.exports = {
-    UserList
-}
+  UserList,
+  MovieList
+};
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -5,13 +5,50 @@ const typeDefs = gql`
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+    favouriteMovies: [Movie]
   }
-  type Query { 
-    users: [User!]!
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheatres: Boolean!
+  }
+
+  type Query {
+    users: [User!]! #this it the query to get the users
+    user(id: ID!): User! #this is the query to get the user by ID
+    movies: [Movie!]! #this is the query to get the movies
+    movie(name: String!): Movie! #this is the query to get the movie by ID
+  }
+
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality = BRAZIL
+  }
+
+  input UpdateUsernameInput {
+    id: ID!
+    newUsername: String!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+    updateUsername(input: UpdateUsernameInput!): User
+    deleteUser(id: ID!): User
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    MEXICO
   }
 `;
 
 module.exports = {
-    typeDefs
-}
+  typeDefs,
+};
